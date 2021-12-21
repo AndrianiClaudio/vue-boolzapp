642c529580995d53253466695deaa70256ce97a5
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-21 11:11:48 +0100

    inserimento dinamico dei messaggi

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente(verdi) e dall’interlocutore(bianco) assegnando due classi CSS diverse
 // Visualizzazione dinamica della lista contatti: tramite la direttiva v -for, visualizzare nome e immagine di ogni contatto
 // Ecco l’array contacts:
+
+
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v -for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
+
 const container = new Vue ({
     el: '#container',
     data: {
         contacts: [],
-        clicked: 2,
+        clicked: 0,
     },
     methods: {
         chanceClicked(i) {
