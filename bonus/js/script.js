53ec883f54a0ae39a83e7a0c217b35b4e51c1e2a
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-25 01:28:25 +0100

    salvataggio bonus fino a invio con click

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,25 +1,42 @@
-// Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente(verdi) e dall’interlocutore(bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v -for, visualizzare nome e immagine di ogni contatto
-// Ecco l’array contacts:
 
 
-// Milestone 2
-// Visualizzazione dinamica dei messaggi: tramite la direttiva v -for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// Click sul contatto mostra la conversazione del contatto cliccato
+// ● predisporre una lista di frasi e / o citazioni da utilizzare al posto della risposta "ok:"
+// quando il pc risponde, anziché scrivere "ok", scegliere una frase random dalla lista e
+// utilizzarla come testo del messaggio di risposta del pc
+// ---------------------------------------------------------------------------------------------
+// ● visualizzare nella lista dei contatti l'ultimo messaggio inviato/ricevuto da ciascun
+// contatto
+// ---------------------------------------------------------------------------------------------
+// ● inserire l'orario corretto nei messaggi
+// ---------------------------------------------------------------------------------------------
+// ● sotto al nome del contatto nella parte in alto a destra, cambiare l'indicazione dello
+// stato: visualizzare il testo "sta scrivendo..." nel timeout in cui il pc risponde, poi
+// mantenere la scritta "online" per un paio di secondi e infine visualizzare "ultimo
+// accesso alle xx:yy" con l'orario corretto
+// ---------------------------------------------------------------------------------------------
+// ● dare la possibilità all'utente di cancellare tutti i messaggi di un contatto o di
+// cancellare l'intera chat con tutti i suoi dati: cliccando sull'icona con i tre pallini in alto a
+// destra, si apre un dropdown menu in cui sono presenti le voci "Elimina messaggi" ed
+// "Elimina chat"; cliccando su di essi si cancellano rispettivamente tutti i messaggi di
+// quel contatto(quindi rimane la conversazione vuota) oppure l'intera chat
+// comprensiva di tutti i dati del contatto oltre che tutti i suoi messaggi(quindi sparisce il
+// contatto anche dalla lista di sinistra)
+// ---------------------------------------------------------------------------------------------
+// ● visualizzare un messaggio di benvenuto che invita l'utente a selezionare un contatto
+// dalla lista per visualizzare i suoi messaggi, anziché attivare di default la prima
+// conversazione
+// ---------------------------------------------------------------------------------------------
+// ● aggiungere una splash page visibile per 1s all'apertura dell'app
+// ---------------------------------------------------------------------------------------------
+// ● aggiungere un'icona per cambiare la modalità light/dark
+// ---------------------------------------------------------------------------------------------
+// ● aggiungere un'icona per ingrandire o rimpicciolire il font
+// ---------------------------------------------------------------------------------------------
 
-// Milestone 3
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
-// Milestone 4
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite(es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
-
-// Milestone 5
-// ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
-// permette di cancellare il messaggio selezionato
-// Visualizzazione ora e ultimo messaggio inviato / ricevuto nella lista dei contatti
-
+// ● evitare che l'utente possa inviare un messaggio vuoto o composto solamente da
+// spazi
+// ---------------------------------------------------------------------------------------------
 const app = new Vue ({
     el: '#app',
     data: {
@@ -99,7 +116,12 @@ const app = new Vue ({
                         family: 'fas',
                         prefix: 'fa-',
                         name: 'microphone',
-                    }
+                    },
+                    {
+                        family: 'fas',
+                        prefix: 'fa-',
+                        name: 'plane',
+                    },
                 ]
             }
         },
@@ -121,16 +143,19 @@ const app = new Vue ({
         // funzione che prende il testo del messaggio e lo pusha nell'araay messaggi corretto
         //devo sapere il contact a cui inviare...
         sendMessage() {
-            const message = {
-                date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-                text: this.main.footer.value,
-                status: "sent",
-                clicked:false,
-                saved: true,
-            };
-            this.contacts[this.clicked].messages.push(message);
-            // reset input value: dopo aver inviato un messaggio, cancello il suo contenuto dalla input
-            this.main.footer.value = '';
+            //trim rimuove gli spazi iniziali e finali
+            this.main.footer.input.value = this.main.footer.input.value.trim();
+            if (this.main.footer.input.value.length > 0) {
+                const message = {
+                    date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                    text: this.main.footer.input.value,
+                    status: "sent",
+                    clicked: false,
+                    saved: true,
+                };
+                this.contacts[this.clicked].messages.push(message);
+                this.main.footer.input.value = '';
+            }
         },
         receivedTimed() {
             function message_received () {
